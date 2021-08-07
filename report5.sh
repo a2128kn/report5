6dbfe8ff3f85083f7ba0300c485144893f9c3a94
a2128kn <82662446+a2128kn@users.noreply.github.com>
2021-08-07 23:24:25 +0900

    Create report5.sh

---
+++ report5.sh
@@ -0,0 +1,24 @@
+
+if [ $# -ne 2 ]
+then
+ echo "使い方 ./report5.sh 自然数1 自然数2"
+ exit 1
+fi
+
+test ()
+{
+ div_a=$1
+ div_b=$2
+ remainder=1
+ until [ "$remainder" -eq 0 ]
+ do
+let "remainder = $div_a % $div_b"
+div_a=$div_b
+div_b=$remainder
+ done
+}
+
+test $1 $2
+echo; echo "$1 と $2 の最大公約数は $div_a"; echo
+
+exit 0
